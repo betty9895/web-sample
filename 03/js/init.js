71fc46f7b9528197dcd2e49d459df3f0f2f5fc1e
ouyuru <betty9895@gmail.com>
2019-11-01 14:23:16 +0800

    fibonacci inital

--- 03/js/init.js
+++ 03/js/init.js
@@ -1,23 +1,77 @@
-const scene=new THREE.Scene();
-const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-scene.add(camera);
-const renderer=new THREE.WebGLRenderer({antialias:true});
+var camera, scene, renderer;
 
-renderer.setSize(window.innerWidth*0.81,window.innerHeight*0.81);
-// what is this
-document.body.appendChild(renderer.domElement);
+var num=40;
+var geo=new Array(num);
+var mesh=new Array(num);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+init();
+animate();
 
-camera.position.z=5;
+function fibonacci(n) {
+	if (n <= 1) {
+		return n;
+	} else {
+		var pre = 0;
+		var cur = 1;
+		var aa = 0;
+		for (var i = 2; i <= n; i+=2) {
+			
+			cur = pre + cur;
+			pre = cur - pre;
+			aa=Math.pow(pre/cur,2)*5;
+			geo[i] = new THREE.BoxGeometry(0.05, 0.05,  0.4*(aa+ 1));
+			mesh[i] = new THREE.Mesh(geo[i], mat);
+			scene.add(mesh[i]);
+			mesh[i].position.set(0,1- 0.05 * i, -1);
+			// console.log(mesh[i].position);
+		}
 
-function animate(){
-    requestAnimationFrame(animate);
-    cube.rotation.y+=0.1;
-    renderer.render(scene,camera);
+		for (var j = 3; j <= n; j+=2) {		
+			geo[j] = new THREE.BoxGeometry(0.05, 0.05, 0.5*(aa + 1));
+			mesh[j] = new THREE.Mesh(geo[j], mat);
+			scene.add(mesh[j]);
+			mesh[j].position.set(0,1- 0.05 * j, -1);
+			// console.log(geo[i].vertices);
+		}
+	}
 }
 
-animate();
+function modelIncrement(){
+	for(var i=0;i<num-1;i++){
+		geo[i]=new THREE.BoxGeometry(0.05,0.05,0.02*(i+1));
+		mesh[i]= new THREE.Mesh( geo[i], mat );
+		scene.add(mesh[i]);
+		mesh[i].position.set(0, 1-0.05*i, -1);
+		console.log(mesh[i].position)
+	}
+}
+
+function init() {
+
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.01, 1000 );
+	camera.position.z = 1;
+	
+	mat = new THREE.MeshNormalMaterial();
+	fibonacci(num);
+	// modelIncrement();
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth * 0.81, window.innerHeight * 0.81 );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	for (var i = 2; i <= num; i+=2) {
+		mesh[i].rotation.y += (i-1)/1000;
+	}
+	for (var j = 3; j <= num; j+=2) {
+		mesh[j].rotation.y += (j-1)/1000;
+	}
+	for (var i = 2; i <= num; i+=2) {
+	}
+	renderer.render( scene, camera );
+
+}
